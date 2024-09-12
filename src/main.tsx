ea24ba029c2c953f2a8b2f9578c68d1b47875b5e
Yerbol Sultanbayev <sultanbayev@gmail.com>
2024-09-12 15:01:12 +0500

    feat: test

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import { App } from "@/app"
+import { App } from "@/app/1"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
